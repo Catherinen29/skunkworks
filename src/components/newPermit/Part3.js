dc5f1a62644c7df31dd42a8fb254c891c219dfb3
Catherinen29 <cat_nevin@hotmail.com>
2023-11-04 16:55:09 +0000

    Create Part3 component.

---
+++ src/components/newPermit/Part3.js
@@ -0,0 +1,19 @@
+import { Box, Button, Checkbox, FormControl, FormControlLabel, FormHelperText, FormLabel, InputBase, InputLabel, MenuItem, Paper, Radio, RadioGroup, Select, TextField, createTheme } from "@mui/material"
+
+
+export default function Part3() {
+
+
+    return(
+        <Paper component="section" sx={{ 
+            width: '70%', /*This doesn't work */
+            bgcolor: '#ffffff',
+            borderRadius: 1,
+            padding: 3,
+            margin: 2 }} >
+        <h3>Part 2 - Duration and Location of Work</h3>
+
+        
+        </Paper>
+    )
+}
