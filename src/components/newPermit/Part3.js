d35366da93238e363bac829a14509b6454f0228e
Catherinen29 <cat_nevin@hotmail.com>
2023-11-04 17:43:01 +0000

    Build out Part3. Date and time fields.

--- src/components/newPermit/Part3.js
+++ src/components/newPermit/Part3.js
@@ -1,8 +1,22 @@
 import { Box, Button, Checkbox, FormControl, FormControlLabel, FormHelperText, FormLabel, InputBase, InputLabel, MenuItem, Paper, Radio, RadioGroup, Select, TextField, createTheme } from "@mui/material"
-
+import { DatePicker } from "@mui/x-date-pickers"
+import { TimePicker } from "@mui/x-date-pickers"
+import { useState } from "react"
+import dayjs from "dayjs"
 
 export default function Part3() {
 
+const [worksDate, setWorksDate] = useState()
+const [startTime, setStartTime] = useState(dayjs('2022-04-17T00:00'))
+const [endTime, setEndTime] = useState()
+
+const handleChangeStart = (e) => {
+    setStartTime(e)
+}
+
+const handleChangeEnd = (e) => {
+    setEndTime(e)
+}
 
     return(
         <Paper component="section" sx={{ 
@@ -11,9 +25,29 @@ export default function Part3() {
             borderRadius: 1,
             padding: 3,
             margin: 2 }} >
-        <h3>Part 2 - Duration and Location of Work</h3>
-
+        <h3>Part 3 - Duration and Location of Work</h3> 
         
+        <label>Set the start and end date of the permit</label>
+        
+        <InputLabel sx={{fontSize: 12}}>Start date</InputLabel>
+        <FormControl>
+            <DatePicker value={worksDate} />
+        </FormControl>
+        
+        <div>
+        <InputLabel sx={{fontSize: 12}}>Start time</InputLabel>
+        <FormControl>
+            <TimePicker value={startTime} onChange={handleChangeStart} />
+        </FormControl>
+        </div>
+
+        <div>
+        <InputLabel sx={{fontSize: 12}}>End time</InputLabel>
+        <FormControl>
+            <TimePicker value={endTime} onChange={handleChangeEnd} />
+        </FormControl>
+        </div>
+
         </Paper>
     )
 }
