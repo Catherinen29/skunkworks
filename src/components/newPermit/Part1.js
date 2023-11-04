ac967cab96a997bf119b78d72393de10d040a700
Catherinen29 <cat_nevin@hotmail.com>
2023-11-04 11:37:33 +0000

    Begin building out Part1.

--- src/components/newPermit/Part1.js
+++ src/components/newPermit/Part1.js
@@ -1,11 +1,30 @@
-
+import { Box, Paper } from "@mui/material"
 
 export default function Part1() {
 
     return(
-        <>
+        <Paper component="section" sx={{ 
+                width: '90%', /*This doesn't work */
+                bgcolor: '#ffffff',
+                borderRadius: 1 }}>
             <h3>Part 1 - Hot Works Permit Details</h3>
+            <div>
+                Permit issuer 
+                {/* information icon */}
+                <Box component="section"
+                    sx={{bgcolor:'#f5f5f5',
+                        width:'50%'
+                }}>Current user 
+                    <div>Verified?</div>
+                </Box>
+            </div>
+
+            <div>
+                Select type of organisation
+                
+            </div>
+
             
-        </>
+        </Paper>
     )
 }
