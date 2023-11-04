c7202adf7b6c906ccd3d928b6427eb761fbdeb50
Catherinen29 <cat_nevin@hotmail.com>
2023-11-04 18:16:39 +0000

    Create Part4 and Part5 components.

---
+++ src/components/newPermit/Part5.js
@@ -0,0 +1,10 @@
+import { Box, Button, Checkbox, FormControl, FormControlLabel, FormHelperText, FormLabel, InputBase, InputLabel, MenuItem, Paper, Radio, RadioGroup, Select, TextField, createTheme } from "@mui/material"
+
+
+export default function Part5() {
+
+    return (
+        <>
+        </>
+    )
+}
