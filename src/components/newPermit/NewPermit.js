44139bc179864c2d184ada1247758e7f0a9dbd21
Catherinen29 <cat_nevin@hotmail.com>
2023-11-04 15:30:31 +0000

    Amend New Permit Box styling.

--- src/components/newPermit/NewPermit.js
+++ src/components/newPermit/NewPermit.js
@@ -1,14 +1,20 @@
 import Part1 from "./Part1";
 import Part2 from "./Part2";
-
+import { Box, Palette } from "@mui/material";
 
 export default function NewPermit() {
 
     return(
-    <>
+    <Box 
+    display='flex'
+    alignItems='center'
+    justifycontent='center'
+    sx={{ bgcolor: '#eef5f5', flexDirection: 'column'}}>
+
     <h1>New Permit</h1>
         <Part1 />
         <Part2 />
-    </>
+        
+    </Box>
     )
 }
