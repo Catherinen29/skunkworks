948ef18d837eeb68b2d1468f79f3fd2cf61687c6
Catherinen29 <cat_nevin@hotmail.com>
2023-11-04 16:00:13 +0000

    Set up Part 2. Add text field.

--- src/components/newPermit/Part2.js
+++ src/components/newPermit/Part2.js
@@ -1,9 +1,27 @@
+import { Box, Button, FormControl, FormHelperText, FormLabel, InputBase, InputLabel, MenuItem, Paper, Select, TextField, createTheme } from "@mui/material"
 
 export default function Part2() {
 
     return (
-        <>
+        <Paper component="section" sx={{ 
+            width: '70%', /*This doesn't work */
+            bgcolor: '#ffffff',
+            borderRadius: 1,
+            padding: 3,
+            margin: 2 }} >
         <h3>Part 2 - Scope of Work</h3>
-        </>
+
+        <div>
+            <label>Description of the Hot Works to be undertaken</label>
+        <FormControl sx={{
+                margin: 2 }}>
+        <InputLabel sx={{fontSize: 12}}>Enter description</InputLabel>
+            <TextField multiline size='small'
+             />
+        </FormControl>
+        </div>
+
+
+        </Paper>
     )
 }
