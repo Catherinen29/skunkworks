5c7bf4b74160156e3a9b9122df7e60c23e59b3db
Catherinen29 <cat_nevin@hotmail.com>
2023-11-04 18:02:52 +0000

    Add select area form.

--- src/components/newPermit/Part3.js
+++ src/components/newPermit/Part3.js
@@ -8,7 +8,8 @@ export default function Part3() {
 
 const [worksDate, setWorksDate] = useState()
 const [startTime, setStartTime] = useState(dayjs('2022-04-17T00:00'))
-const [endTime, setEndTime] = useState()
+const [endTime, setEndTime] = useState(dayjs('2022-04-17T00:00'))
+const [area, setArea] = useState('')
 
 const handleChangeStart = (e) => {
     setStartTime(e)
@@ -16,6 +17,10 @@ const handleChangeStart = (e) => {
 
 const handleChangeEnd = (e) => {
     setEndTime(e)
+}
+
+const handleChangeArea = (e) => {
+    setArea(e.target.value)
 }
 
     return(
@@ -27,27 +32,50 @@ const handleChangeEnd = (e) => {
             margin: 2 }} >
         <h3>Part 3 - Duration and Location of Work</h3> 
         
+
+        <Box component="div" sx={{display: 'inline'}}>
         <label>Set the start and end date of the permit</label>
         
         <InputLabel sx={{fontSize: 12}}>Start date</InputLabel>
-        <FormControl>
+        <FormControl required size='small' sx={{display: 'block',
+                margin: 2 }}>
             <DatePicker value={worksDate} />
         </FormControl>
         
-        <div>
+
         <InputLabel sx={{fontSize: 12}}>Start time</InputLabel>
-        <FormControl>
+        <FormControl required size='small' sx={{display: 'block',
+                margin: 2 }}>
             <TimePicker value={startTime} onChange={handleChangeStart} />
         </FormControl>
-        </div>
 
-        <div>
+
         <InputLabel sx={{fontSize: 12}}>End time</InputLabel>
-        <FormControl>
+        <FormControl required size='small' sx={{display: 'block',
+                margin: 2 }}>
             <TimePicker value={endTime} onChange={handleChangeEnd} />
         </FormControl>
-        </div>
+        </Box>
 
+
+        <div>
+        
+        <FormControl required size='small' sx={{display: 'block',
+                margin: 2 }}>
+            <InputLabel sx={{fontSize: 12}}>Select area</InputLabel>
+            <Select
+                value={area}
+                label="Select area *"
+                onChange={handleChangeArea}
+                sx={{width: '50%'}}
+                > 
+                <MenuItem value="Hospital Block A">Hospital Block A</MenuItem>
+                <MenuItem value="Hospital Block B">Hospital Block B</MenuItem>
+                <MenuItem value="Hospital Block C">Hospital Block C</MenuItem>
+                <MenuItem value="Hospital Block D">Hospital Block D</MenuItem>           
+            </Select>
+        </FormControl>
+        </div>
         </Paper>
     )
 }
