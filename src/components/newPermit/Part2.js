0b4700cd97c8900fd6cf9afb18aa591b3de6091b
Catherinen29 <cat_nevin@hotmail.com>
2023-11-04 16:47:13 +0000

    Add types of activity form. Check boxes and functionality.

--- src/components/newPermit/Part2.js
+++ src/components/newPermit/Part2.js
@@ -1,6 +1,22 @@
-import { Box, Button, FormControl, FormHelperText, FormLabel, InputBase, InputLabel, MenuItem, Paper, Select, TextField, createTheme } from "@mui/material"
+import { Box, Button, Checkbox, FormControl, FormControlLabel, FormHelperText, FormLabel, InputBase, InputLabel, MenuItem, Paper, Radio, RadioGroup, Select, TextField, createTheme } from "@mui/material"
+import { useState } from "react"
 
 export default function Part2() {
+
+const [selectedActivity, setSelectedActivity] = useState([''])
+
+const updateActivities = (e) => {
+    if (!selectedActivity.includes(e.target.value)) {
+        setSelectedActivity([...selectedActivity, e.target.value]) 
+        console.log(selectedActivity)
+    } else {
+        const index = selectedActivity.indexOf(e.target.value)
+        setSelectedActivity(selectedActivity.splice(index, 1))
+        console.log(selectedActivity)
+
+    }
+
+}
 
     return (
         <Paper component="section" sx={{ 
@@ -18,6 +34,24 @@ export default function Part2() {
         <InputLabel sx={{fontSize: 12}}>Enter description</InputLabel>
             <TextField multiline size='small'
              />
+        </FormControl>
+        </div>
+
+        <div>
+        <FormControl>
+            <label>Select types of activity covered by the permit</label>
+        <FormLabel></FormLabel>
+        <RadioGroup>
+            <FormControlLabel value="Welding inc.Thermic" control={<Checkbox onClick={updateActivities} />} label="Welding inc.Thermic" />
+            <FormControlLabel value="Soldering" control={<Checkbox onClick={updateActivities} />} label="Soldering" />            
+            <FormControlLabel value="Brazing" control={<Checkbox onClick={updateActivities} />} label="Brazing" />
+            <FormControlLabel value="Heathland Clearing" control={<Checkbox onClick={updateActivities} />} label="Heathland Clearing" />            
+            <FormControlLabel value="Burning or Melting" control={<Checkbox onClick={updateActivities} />} label="Burning or Melting" />  
+            <FormControlLabel value="Gas cutting" control={<Checkbox onClick={updateActivities} />} label="Gas cutting" />
+            <FormControlLabel value="Disc cutting/grinding" control={<Checkbox onClick={updateActivities} />} label="Disc cutting/grinding" />
+            <FormControlLabel value="Hot Riveting" control={<Checkbox onClick={updateActivities} />} label="Hot Riveting" />
+
+        </RadioGroup>
         </FormControl>
         </div>
 
