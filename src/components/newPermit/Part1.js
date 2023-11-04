11f19c936eb2421e4177656b840fd0f3b3f9a2b3
Catherinen29 <cat_nevin@hotmail.com>
2023-11-04 12:31:10 +0000

    Add form and organisation drop down.

--- src/components/newPermit/Part1.js
+++ src/components/newPermit/Part1.js
@@ -1,28 +1,47 @@
-import { Box, Paper } from "@mui/material"
+import { Box, FormControl, FormHelperText, MenuItem, Paper, Select } from "@mui/material"
+import { useState } from "react"
 
 export default function Part1() {
+
+const [organisationType, setOrganisationType] = useState("")
+
+const handleOrganisation = (e) => {
+    setOrganisationType(e.target.value)
+}
+
 
     return(
         <Paper component="section" sx={{ 
                 width: '90%', /*This doesn't work */
                 bgcolor: '#ffffff',
-                borderRadius: 1 }}>
-            <h3>Part 1 - Hot Works Permit Details</h3>
-            <div>
-                Permit issuer 
-                {/* information icon */}
-                <Box component="section"
-                    sx={{bgcolor:'#f5f5f5',
-                        width:'50%'
-                }}>Current user 
-                    <div>Verified?</div>
-                </Box>
-            </div>
+                borderRadius: 1,
+                padding: 3 }}>
+        <h3>Part 1 - Hot Works Permit Details</h3>
+        <div>
+            Permit issuer 
+            {/* information icon */}
+            <Box component="section"
+                sx={{bgcolor:'#f5f5f5',
+                    width:'50%'
+            }}>Current user 
+                <div>Verified?</div>
+            </Box>
+        </div>
 
-            <div>
-                Select type of organisation
+        <FormControl required>
+        Select type of organisation
+            <Select
+            value={organisationType}
+            label="Select Type *"
+            onChange={handleOrganisation}> 
+                <MenuItem value="Supplier">Supplier</MenuItem>
+                <MenuItem value="Plumbing">Plumbing</MenuItem>
+                <MenuItem value="Carpentry">Carpentry</MenuItem>                
+                <MenuItem value="Face Painting">Face Painting</MenuItem>
+            </Select>
+        </FormControl>
                 
-            </div>
+
 
             
         </Paper>
