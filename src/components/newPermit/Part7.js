74e3817076d9d44be568541b442c20948d407561
Catherinen29 <cat_nevin@hotmail.com>
2023-11-04 18:50:12 +0000

    Create Part7 component.

---
+++ src/components/newPermit/Part7.js
@@ -0,0 +1,18 @@
+import { Box, Button, Checkbox, FormControl, FormControlLabel, FormHelperText, FormLabel, InputBase, InputLabel, MenuItem, Paper, Radio, RadioGroup, Select, TextField, createTheme } from "@mui/material"
+import { useState } from "react"
+
+
+export default function Part6() {
+
+    return (
+        <Paper component="section" sx={{ 
+            width: '70%', /*This doesn't work */
+            bgcolor: '#ffffff',
+            borderRadius: 1,
+            padding: 3,
+            margin: 2 }} >
+        <h3>Part 7 - Declarations</h3> 
+        
+        </Paper>
+    )
+}
