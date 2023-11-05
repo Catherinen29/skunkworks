8c35878085cc2a6caee7e8612af901a8181e6658
Catherinen29 <cat_nevin@hotmail.com>
2023-11-05 19:53:38 +0000

    Insert button.

--- src/components/newPermit/NewPermit.js
+++ src/components/newPermit/NewPermit.js
@@ -1,12 +1,14 @@
 import Part1 from "./Part1";
 import Part2 from "./Part2";
-import { Box, Palette, Typography, AppBar, Toolbar } from "@mui/material";
+import { Box, Button, Palette, Typography, AppBar, Toolbar } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
 import Part3 from "./Part3";
 import Part4 from "./Part4";
 import Part5 from "./Part5";
 import Part6 from "./Part6";
 import Part7 from "./Part7";
 import PermitIntro from "./PermitIntro";
+import { Close } from "@mui/icons-material";
 // import Image from "src/resources/AA050432-1DE6-4D57-A882-0AE636B7EA26.png"
 
 export default function NewPermit() {
@@ -14,11 +16,11 @@ export default function NewPermit() {
     return(
 <>
 
-    <AppBar>
         <Toolbar sx={{bgcolor:  '#04535f'}}>
-        <Typography>New Permit</Typography>
+        <Button >
+            {<CloseIcon sx={{color: '#ffffff'}} />} </Button>
+        <Typography sx={{color: '#ffffff', fontSize: 20}}>New Permit</Typography>
         </Toolbar>
-    </AppBar>
 
     <Box 
     display='flex'
