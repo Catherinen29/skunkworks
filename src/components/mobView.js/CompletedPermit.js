72a8e3ce42ca0e718dfdb98102de3c7f2ee43282
Catherinen29 <cat_nevin@hotmail.com>
2023-11-15 16:41:31 +0000

    Build out CompletedPermit component. Add snackbar.

--- src/components/mobView.js/CompletedPermit.js
+++ src/components/mobView.js/CompletedPermit.js
@@ -1,19 +1,30 @@
-import { Badge, Box, Button, Card, Typography } from "@mui/material";
+import { Alert, Badge, Box, Button, Card, Snackbar, Typography } from "@mui/material";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useNavigate } from "react-router-dom";
 import CheckCircleIcon from '@mui/icons-material/CheckCircle'
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import NewspaperIcon from '@mui/icons-material/Newspaper';
 import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import ApartmentIcon from '@mui/icons-material/Apartment';
 import PersonIcon from '@mui/icons-material/Person';
-import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import WorkIcon from '@mui/icons-material/Work';
+import FlagCircleIcon from '@mui/icons-material/FlagCircle';
+import DirectionsWalkIcon from '@mui/icons-material/DirectionsWalk';
+import CloseIcon from '@mui/icons-material/Close';
 import mob_bg from '../../mob_bg.png'
+import BB_bg from '../../BB_bg.png'
+import { useState } from "react";
+import { CheckCircleOutline } from "@mui/icons-material";
 
 export default function CompletedPermit() {
 
 const navigate = useNavigate()
 
+const [open, setOpen] = useState(true)
+const handleClose = () => {
+    setOpen(false)
+}
 
 return (
     
@@ -22,52 +33,43 @@ return (
         
     
         {/* Toolbar */}
-        <Box sx={{width: '100%', py: '1rem',
-            backgroundImage: `url(${mob_bg})`, color: 'white', 
+        <Box sx={{width: '100%', pb: '1rem',
+            backgroundImage: `url(${mob_bg})`, backgroundSize: '100%', color: 'white', 
             display: 'flex', flexDirection: 'column', alignItems: 'flex-start', boxShadow: 5}}>
             
             <Box sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', 
-                width: '100%', 
-                mb: '1rem'}}>
+                width: '90%', m: '1rem'}}>
                 <Button onClick={() => navigate('/permits')} sx={{color: 'white'}}>
-                    <ArrowBackIcon />
+                    <ArrowBackIcon sx={{height: '3rem', width: '3rem'}} />
                 </Button>    
             
                 <Button sx={{color: 'white'}}>
                     <Badge badgeContent={1} color="error">
-                        <NotificationsIcon />
+                        <NotificationsIcon sx={{height: '3rem', width: '3rem'}} />
                     </Badge>
                 </Button>
-                
             </Box>
             
             <Box sx={{mx: '1rem'}}>
                <Box sx={{
-                display: 'flex', 
+                display: 'flex', alignItems: 'center',
                 whiteSpace: 'nowrap', mr: '1rem', 
-                    // textOverflow: 'ellipsis',
-                    // overflow: 'hidden',
-                    // p: 1,
-                    fontSize: 18}}> 
-               <ApartmentIcon sx={{mr: '1rem'}} /> 
-                {/* <Box component='div' 
-                    sx={{
-                        textOverflow: 'ellipsis',
-                        overflow: 'hidden'}}> */}
-                    <Typography noWrap sx={{mx: '1rem', fontSize: 18, fontWeight: 500}}>
+                fontSize: 18}}> 
+               <ApartmentIcon sx={{height: '3rem', width: '3rem'}} /> 
+                    <Typography noWrap variant="h4" 
+                        sx={{mx: '1rem', fontWeight: 500}}>
                     NHS Southampton University Hospital
                     </Typography>
-                {/* </Box> */}
                 
                 </Box>
 
-                <Box sx={{display: 'flex'}}>
-                    <PersonIcon />
-                    <Typography sx={{ml: '1rem'}}>James Worthington</Typography>
+                <Box sx={{display: 'flex', alignItems: 'center', my: '0.5rem'}}>
+                    <PersonIcon sx={{height: '3rem', width: '3rem'}} />
+                    <Typography sx={{ml: '1rem', fontSize: 20}}>James Worthington</Typography>
                 </Box>
-                <Box sx={{display: 'flex'}}>
-                    <WorkOutlineIcon />
-                    <Typography sx={{ml: '1rem'}}>Welder</Typography> 
+                <Box sx={{display: 'flex', alignItems: 'center', my: '0.5rem'}}>
+                    <WorkIcon sx={{height: '3rem', width: '3rem'}} />
+                    <Typography sx={{ml: '1rem', fontSize: 20}}>Welder</Typography> 
                 </Box>
             </Box>
             
@@ -76,22 +78,23 @@ return (
         <Box sx={{bgcolor: '#eef5f5'}}>
 
             {/* Verfied */}
-            <Box sx={{m: '2rem', px: '1rem',
+            <Box sx={{m: '1rem', p: '1rem',
                 height: '3rem', display: 'flex', alignItems: 'center',
                 bgcolor: '#4caf50', color: 'white', borderRadius: 1}}>
-                <CheckCircleIcon />
-                <Typography sx={{ml: '0.5rem', fontSize: 14}}>You're fully verified</Typography>
+                <CheckCircleIcon sx={{height: '2rem', width: '2rem'}} />
+                <Typography sx={{ml: '0.5rem', fontSize: 18}}>You're fully verified</Typography>
             </Box>
 
 
             {/* Tasks */}
-            <Box sx={{m: '2rem'}}>
+            <Box sx={{m: '1rem'}}>
                 <Card sx={{my: '1rem', bgcolor: '#04535f', color: 'white'}}>
                     <Box sx={{display: 'flex', flexDirection: 'column',
-                        ml: '1rem', mr: '1.5rem', my: '2rem'}}>
-                    <Typography sx={{mb: '1rem'}}>Tasks</Typography>
+                        // ml: '1rem', mr: '1.5rem', my: '2rem'
+                        m: '2rem'}}>
+                    <Typography variant="h4" sx={{mb: '1rem', fontWeight: 500}}>Tasks</Typography>
                     <Typography paragraph 
-                        sx={{color: 'rgba(255, 255, 255, 0.7)', fontSize: 14}}>
+                        sx={{color: 'rgba(255, 255, 255, 0.7)'}}>
                         Specific project tasks assigned to you</Typography>
                     </Box>
 
@@ -104,13 +107,13 @@ return (
                 <Typography variant="h5">
                     My Wallet</Typography>
 
-            <Box sx={{p: 0, my: '1rem', display: 'flex', flexFlow: 'row wrap', gap: '2rem'}}>
+            <Box sx={{p: 0, my: '1rem', display: 'flex', flexFlow: 'row wrap', gap: '1rem'}}>
                 
                     <Card sx={{px: '1rem', width: '40%', height: '15rem', position: 'relative'}}>
                         
                         <Box sx={{width: '2rem', height: '2rem', my: '1rem',
                             display: 'flex', justifyContent: 'center', alignItems: 'center',
-                             borderRadius: 1, bgcolor: '#00a4a9', color: 'white'}}>
+                            borderRadius: 1, bgcolor: '#00a4a9', color: 'white'}}>
                             <AssignmentIndIcon sx={{height: '1.8rem', width: '1.8rem'}} />    
                         </Box>
                         
@@ -128,7 +131,7 @@ return (
                         
                         <Box sx={{width: '2rem', height: '2rem', my: '1rem',
                             display: 'flex', justifyContent: 'center', alignItems: 'center',
-                             borderRadius: 1, bgcolor: '#00a4a9', color: 'white'}}>
+                            borderRadius: 1, bgcolor: '#00a4a9', color: 'white'}}>
                                 <NewspaperIcon sx={{height: '1.8rem', width: '1.8rem'}} />
                         </Box>
                         
@@ -140,21 +143,69 @@ return (
                             sx={{position: 'absolute', bottom: 0, right: 0, m: '0.5rem'}}>VIEW</Button>
                     </Card>
 
-                    <Card sx={{p: '1rem', width: '40%', height: '15rem'}}>
-                        <Typography variant='h5' sx={{fontWeight: 500}}>Other</Typography>
+                    <Card sx={{px: '1rem', width: '40%', height: '15rem', position: 'relative'}}>
+                        <Box sx={{width: '2rem', height: '2rem', my: '1rem',
+                            display: 'flex', justifyContent: 'center', alignItems: 'center',
+                            borderRadius: 1, bgcolor: '#00a4a9', color: 'white'}}>
+                                <FlagCircleIcon sx={{height: '1.8rem', width: '1.8rem'}} />
+                        </Box>
+                        
+                        <Typography variant='h5' sx={{fontWeight: 500}}>Flags</Typography>
+                        <Typography sx={{color: 'rgba(0, 0, 0, 0.6)'}}>
+                            Health & Safety details
+                        </Typography> 
+                        <Button
+                            sx={{position: 'absolute', bottom: 0, right: 0, m: '0.5rem'}}>VIEW</Button>
                     </Card>
 
-                    <Card sx={{p: '1rem', width: '40%', height: '15rem'}}>
+                    <Card sx={{px: '1rem', width: '40%', height: '15rem', position: 'relative'}}>
+                        <Box sx={{width: '2rem', height: '2rem', my: '1rem',
+                            display: 'flex', justifyContent: 'center', alignItems: 'center',
+                            borderRadius: 1, bgcolor: '#00a4a9', color: 'white'}}>
+                                <DirectionsWalkIcon sx={{height: '1.8rem', width: '1.8rem'}} />
+                        </Box>
                         <Typography variant='h5' sx={{fontWeight: 500}}>Other</Typography>
+                        <Typography sx={{color: 'rgba(0, 0, 0, 0.6)'}}>
+                            Permit deadlines
+                        </Typography>
+                        <Button
+                            sx={{position: 'absolute', bottom: 0, right: 0, m: '0.5rem'}}>VIEW</Button>
                     </Card>        
                     
             </Box>
 
-
             </Box>
 
-            </Box>   
-    
+        </Box>   
+
+        <Box>
+            <Snackbar open={open} onClose={handleClose} 
+                // autoHideDuration={6000}
+                anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "center"
+                    }}
+                sx={{width: '80%'}}
+            >
+            <Alert 
+                sx={{color: 'white', bgcolor: 'grey', 
+                display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'
+                }}
+                iconMapping={{
+                success: <CheckCircleOutlineIcon fontSize="inherit" sx={{color: 'white'}} />,
+                }}
+            >
+                <Box sx={{display: 'flex', flexDirection: 'row'}}>
+                    <Typography>You have completed all your tasks on 
+                        the Hot Work permit. 
+                    </Typography>
+                    <CloseIcon sx={{ml: '1rem'}} />    
+                </Box>
+                
+            </Alert>
+                        
+            </Snackbar>
+        </Box>
     </Card>
                 
     )
