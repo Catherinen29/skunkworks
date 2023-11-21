296e3975ca603906d9b56910e6c044b9a9edf016
Catherinen29 <cat_nevin@hotmail.com>
2023-11-21 12:21:25 +0000

    Refactoring.

--- src/components/PermitSignOff/GoldenThreadTimeline.js
+++ src/components/PermitSignOff/GoldenThreadTimeline.js
@@ -1,6 +1,7 @@
-import { Box, Button, Divider, Typography } from "@mui/material";
+import { Box, Button, Divider, Stack, Typography } from "@mui/material";
 import { Timeline, TimelineConnector, TimelineContent, 
     TimelineDot, TimelineItem, TimelineSeparator } from "@mui/lab";
+import { timelineItemClasses } from '@mui/lab/TimelineItem'
 import NewspaperIcon from '@mui/icons-material/Newspaper';
 import CloseIcon from '@mui/icons-material/Close';
 import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
@@ -14,7 +15,7 @@ const handleClose = () => {
 
 
 return (
-<Box>
+<>
     
     {/* Title */}
     <Box sx={{p: '2rem', pb: '1rem'}}>
@@ -45,180 +46,230 @@ return (
 
         <Divider />
 
-    <Box>
 
-    
-    <Timeline>
-    
-        <Typography 
-            sx={{fontSize: 12, color: 'rgba(0, 0, 0, 0.6)', mt: '1rem', mx: '2rem'}}>
-            1 Nov 23</Typography>    
-        <TimelineItem sx={{ml: '-15rem'}}>
+    <Box sx={{ overflow: 'auto' }}>
+
+    <Stack direction="column" gap={2}>
+        <Typography variant="caption" sx={{mx: 2, mt: 2}}>1 Nov 23</Typography>
+
+        <Timeline 
+            sx={{p: 0, m: 0,
+                [`& .${timelineItemClasses.root}:before`]: {
+                    flex: 0,
+                    padding: 0,
+                  }
+                }}>
+            
+        <TimelineItem sx={{mx: 1}}>
             <TimelineSeparator>
                 <CheckCircleOutlineRoundedIcon sx={{fontSize: 25, my: '0.5rem', color: '#4caf50'}} />
-                <TimelineConnector sx={{width: '3%'}} />
-                </TimelineSeparator>
-                <TimelineContent sx={{display: 'flex'}}>   
-                    <Box sx={{display: 'flex', flexDirection: 'column'}}>
-                        <Typography>Hot Works Permit completed </Typography>
-                        <Typography sx={{fontSize: 12, color: 'rgba(0, 0, 0, 0.6)'}}>Automated - Certchain Permits</Typography>
-                    </Box>
-                    <ArrowForwardIosRoundedIcon sx={{fontSize: 14, mr: '1rem'}} />
-                </TimelineContent>
+                <TimelineConnector sx={{ height: '37px' }} />
+            </TimelineSeparator>
+            <TimelineContent sx={{display: 'flex', justifyContent: 'space-between'}}>   
+                <Stack direction="column" gap={0.5}>
+                    <Typography variant="body1">Hot Works Permit completed </Typography>
+                    <Typography variant="caption">Automated - Certchain Permits</Typography>
+                </Stack>
+                <ArrowForwardIosRoundedIcon sx={{fontSize: 14, m: '1rem'}} />
+            </TimelineContent>
         </TimelineItem>
 
-        <TimelineItem sx={{ml: '-15rem'}}>
+        <TimelineItem sx={{mx: 2}}>
             <TimelineSeparator>
                 <TimelineDot />
-                <TimelineConnector />                 
+                <TimelineConnector sx={{ height: '37px' }} />                 
             </TimelineSeparator>
-                <TimelineContent>
-                    Issuer sign-off completed
-                    <Typography sx={{fontSize: 12, color: 'rgba(0, 0, 0, 0.6)'}}>Francis Golder | Balfour Beatty</Typography>
-                    <ArrowForwardIosRoundedIcon sx={{fontSize: 14, mr: '1rem'}} />
-                </TimelineContent>
+            <TimelineContent sx={{display: 'flex', justifyContent: 'space-between'}}>
+                <Stack direction="column" gap={0.5} >
+                <Typography variant="body1">Issuer sign-off completed</Typography>
+                <Typography variant="caption">Francis Golder | Balfour Beatty</Typography>
+                </Stack>
+                <ArrowForwardIosRoundedIcon sx={{fontSize: 14, m: '1rem'}} />
+            </TimelineContent>
         </TimelineItem>
 
-        <TimelineItem sx={{ml: '-15rem'}}>
+        <TimelineItem sx={{mx: 2}}>
             <TimelineSeparator>
                 <TimelineDot />
-                <TimelineConnector />                 
+                <TimelineConnector sx={{ height: '37px' }} />                 
             </TimelineSeparator>
-                <TimelineContent>
-                    <Typography>Operator sign-off completed</Typography>
-                    <Typography sx={{fontSize: 12, color: 'rgba(0, 0, 0, 0.6)'}}>Jackie Mason | Ackrington Chambers</Typography>
-                    <ArrowForwardIosRoundedIcon sx={{fontSize: 14, mr: '1rem'}} />
-                </TimelineContent>
+            <TimelineContent sx={{display: 'flex', justifyContent: 'space-between'}}>
+                <Stack direction="column" gap={0.5}>
+                <Typography variant="body1">Operator sign-off completed</Typography>
+                <Typography variant="caption" 
+                    sx={{color: 'rgba(0, 0, 0, 0.6)'}}>
+                        Jackie Mason | Ackrington Chambers</Typography>
+                </Stack>
+                <ArrowForwardIosRoundedIcon sx={{fontSize: 14, m: '1rem'}} />
+            </TimelineContent>
         </TimelineItem>
 
-        <TimelineItem sx={{ml: '-15rem'}}>
+        <TimelineItem sx={{mx: 2}}>
             <TimelineSeparator>
                 <TimelineDot />
-                <TimelineConnector />                 
+                <TimelineConnector sx={{ height: '37px' }} />                 
             </TimelineSeparator>
-                <TimelineContent>
-                    <Typography>Fire Safety sign-off completed</Typography>
-                    <Typography sx={{fontSize: 12, color: 'rgba(0, 0, 0, 0.6)'}}>Jackie Mason | Ackrington Chambers</Typography>
-
-                <ArrowForwardIosRoundedIcon sx={{fontSize: 14, mr: '1rem'}} />
+            <TimelineContent sx={{display: 'flex', justifyContent: 'space-between'}}>
+                <Stack direction="column" gap={0.5}>
+                <Typography variant="body1">Fire Safety sign-off completed</Typography>
+                <Typography variant="caption">Jackie Mason | Ackrington Chambers</Typography>
+                </Stack>
+            <ArrowForwardIosRoundedIcon sx={{fontSize: 14, m: '1rem'}} />
         </TimelineContent>
         </TimelineItem>
 
-        <TimelineItem sx={{ml: '-15rem'}}>
+        <TimelineItem sx={{mx: 1}}>
             <TimelineSeparator>
                 <CheckCircleOutlineRoundedIcon sx={{fontSize: 25, mx: -0.3, my: '0.5rem', color: '#4caf50'}} />
-                <TimelineConnector sx={{width: '13%'}} />                 
+                <TimelineConnector sx={{ height: '37px' }} />                 
             </TimelineSeparator>
-                <TimelineContent>
-                    <Typography>Hot Works permit active</Typography>
-                    <Typography sx={{fontSize: 12, color: 'rgba(0, 0, 0, 0.6)'}}>Automated - Certchain permits</Typography>
-           
-                    <ArrowForwardIosRoundedIcon sx={{fontSize: 14, mr: '1rem'}} />
-        </TimelineContent>
+            <TimelineContent sx={{display: 'flex', justifyContent: 'space-between'}}>
+                <Stack direction="column" gap={0.5}>
+                <Typography variant="body1">Hot Works permit active</Typography>
+                <Typography variant="caption"
+                    sx={{fontSize: 12, color: 'rgba(0, 0, 0, 0.6)'}}
+                    >Automated - Certchain permits</Typography>
+                </Stack>
+                <ArrowForwardIosRoundedIcon sx={{fontSize: 14, m: '1rem'}} />
+            </TimelineContent>
         </TimelineItem>
 
-        <TimelineItem sx={{ml: '-15rem'}}>
+        <TimelineItem sx={{mx: 2}}>
             <TimelineSeparator>
                 <TimelineDot />
-                <TimelineConnector />                 
+                <TimelineConnector sx={{ height: '37px' }} />                 
             </TimelineSeparator>
-                <TimelineContent>
-                    <Typography>Hot Works permit authorised</Typography>
-                    <Typography sx={{fontSize: 12, color: 'rgba(0, 0, 0, 0.6)'}}>Francis Golder | Balfour Beatty</Typography>
-            
-                    <ArrowForwardIosRoundedIcon sx={{fontSize: 14, mr: '1rem'}} />
+            <TimelineContent sx={{display: 'flex', justifyContent: 'space-between'}}>
+                <Stack direction="column" gap={0.5}>
+                <Typography variant="body1">Hot Works permit authorised</Typography>
+                <Typography variant="caption"
+                    sx={{color: 'rgba(0, 0, 0, 0.6)'}}
+                        >Francis Golder | Balfour Beatty</Typography>
+                </Stack>
+                <ArrowForwardIosRoundedIcon sx={{fontSize: 14, m: '1rem'}} />
         </TimelineContent>
         </TimelineItem>
 
-        <TimelineItem sx={{ml: '-15rem'}}>
+        <TimelineItem sx={{mx: 2}}>
             <TimelineSeparator>
                 <TimelineDot />
-                <TimelineConnector />                 
+                <TimelineConnector sx={{ height: '37px' }} />                 
             </TimelineSeparator>
-                <TimelineContent>
-                    <Typography>Precaution checklist completed</Typography>
-                    <Typography sx={{fontSize: 12, color: 'rgba(0, 0, 0, 0.6)'}}>James Worthington | Ackrington Chambers</Typography>
-                
-                    <ArrowForwardIosRoundedIcon sx={{fontSize: 14, mr: '1rem'}} />
-        </TimelineContent>
+            <TimelineContent sx={{display: 'flex', justifyContent: 'space-between'}}>
+                <Stack direction="column" gap={0.5}>
+                <Typography variant="body1">Precaution checklist completed</Typography>
+                <Typography variant="caption"
+                //  sx={{fontSize: 12, color: 'rgba(0, 0, 0, 0.6)'}}
+                    >James Worthington | Ackrington Chambers</Typography>
+                </Stack>
+                <ArrowForwardIosRoundedIcon sx={{fontSize: 14, m: '1rem'}} />
+            </TimelineContent>
         </TimelineItem>
+    </Timeline>
 
-    <Typography 
-        sx={{fontSize: 12, color: 'rgba(0, 0, 0, 0.6)', mt: '1rem', mx: '1.5rem'}}>
+    <Typography  variant="caption" sx={{mx: 2}}>
         30 Oct 23</Typography>
-
-        <TimelineItem sx={{ml: '-15rem'}}>
+    <Timeline 
+            sx={{p: 0, m: 0,
+                [`& .${timelineItemClasses.root}:before`]: {
+                    flex: 0,
+                    padding: 0,
+                  }
+                }}>
+        <TimelineItem sx={{mx: 2}}>
             <TimelineSeparator>
                 <TimelineDot />
-                <TimelineConnector />                 
+                <TimelineConnector sx={{ height: '37px' }} />                 
             </TimelineSeparator>
-                <TimelineContent>
-                    <Typography>Issuer signature completed</Typography>
-                    <Typography sx={{fontSize: 12, color: 'rgba(0, 0, 0, 0.6)'}}>Barry White | Balfour Beatty</Typography>
-                
-                    <ArrowForwardIosRoundedIcon sx={{fontSize: 14, mr: '1rem'}} />
-        </TimelineContent>
+            <TimelineContent sx={{display: 'flex', justifyContent: 'space-between'}}>
+                <Stack direction="column" gap={0.5}>
+                <Typography variant="body1">Issuer signature completed</Typography>
+                <Typography variant="caption" 
+                    // sx={{fontSize: 12, color: 'rgba(0, 0, 0, 0.6)'}}
+                    >Barry White | Balfour Beatty</Typography>
+                </Stack>
+                <ArrowForwardIosRoundedIcon sx={{fontSize: 14, m: '1rem'}} />
+            </TimelineContent>
         </TimelineItem>
 
-        <TimelineItem sx={{ml: '-15rem'}}>
+        <TimelineItem sx={{mx: 2}}>
             <TimelineSeparator>
                 <TimelineDot />
-                <TimelineConnector />                 
+                <TimelineConnector sx={{ height: '37px' }} />                 
             </TimelineSeparator>
-                <TimelineContent>
-                    <Typography>Supervisor signature completed</Typography>
-                    <Typography sx={{fontSize: 12, color: 'rgba(0, 0, 0, 0.6)'}}>Barry White | Balfour Beatty</Typography>
-                
-                    <ArrowForwardIosRoundedIcon sx={{fontSize: 14, mr: '1rem'}} />
-        </TimelineContent>
+            <TimelineContent sx={{display: 'flex', justifyContent: 'space-between'}}>
+                <Stack direction="column" gap={0.5}>
+                <Typography variant="body1">Supervisor signature completed</Typography>
+                <Typography  variant="caption"
+                // sx={{color: 'rgba(0, 0, 0, 0.6)'}}
+                >Barry White | Balfour Beatty</Typography>
+                </Stack>
+                <ArrowForwardIosRoundedIcon sx={{fontSize: 14, m: '1rem'}} />
+            </TimelineContent>
         </TimelineItem>
 
-        <TimelineItem sx={{ml: '-15rem'}}>
+        <TimelineItem sx={{mx: 2}}>
             <TimelineSeparator>
                 <TimelineDot />
-                <TimelineConnector />                 
+                <TimelineConnector sx={{ height: '37px' }} />                 
             </TimelineSeparator>
-                <TimelineContent>
-                    <Typography>Operator signature completed</Typography>
-                    <Typography sx={{fontSize: 12, color: 'rgba(0, 0, 0, 0.6)'}}>Barry White | Balfour Beatty</Typography>
-                
-                    <ArrowForwardIosRoundedIcon sx={{fontSize: 14, mr: '1rem'}} />
-        </TimelineContent>
+            <TimelineContent sx={{display: 'flex', justifyContent: 'space-between'}}>
+                <Stack direction="column" gap={0.5}>
+                <Typography variant="body1">Operator signature completed</Typography>
+                <Typography variant="caption" 
+                    // sx={{color: 'rgba(0, 0, 0, 0.6)'}}
+                    >Barry White | Balfour Beatty</Typography>
+                </Stack>
+                <ArrowForwardIosRoundedIcon sx={{fontSize: 14, m: '1rem'}} />
+            </TimelineContent>
         </TimelineItem>
 
-        <TimelineItem sx={{ml: '-15rem'}}>
+        <TimelineItem sx={{mx: 2}}>
             <TimelineSeparator>
                 <TimelineDot />
-                <TimelineConnector />                 
+                <TimelineConnector sx={{ height: '37px' }} />                 
             </TimelineSeparator>
-                <TimelineContent>
-                    <Typography>Risk assessment compelted</Typography>
-                    <Typography sx={{fontSize: 12, color: 'rgba(0, 0, 0, 0.6)'}}>Barry White | Balfour Beatty</Typography>
-                
-                    <ArrowForwardIosRoundedIcon sx={{fontSize: 14, mr: '1rem'}} />
-        </TimelineContent>
+            <TimelineContent sx={{display: 'flex', justifyContent: 'space-between'}}>
+                <Stack direction="column" gap={0.5}>
+                <Typography variant="body1">Risk assessment compelted</Typography>
+                <Typography variant="caption" 
+                    // sx={{color: 'rgba(0, 0, 0, 0.6)'}}
+                    >Barry White | Balfour Beatty</Typography>
+                </Stack>
+                <ArrowForwardIosRoundedIcon sx={{fontSize: 14, m: '1rem'}} />
+            </TimelineContent>
         </TimelineItem>
+    </Timeline>
 
-    <Typography 
-        sx={{fontSize: 12, color: 'rgba(0, 0, 0, 0.6)', mt: '1rem', mb: '0.5rem', mx: '1.5rem'}}>
+    <Typography variant="caption" sx={{mx: 2}}>
         13 Oct 23</Typography>
-
-        <TimelineItem sx={{ml: '-15rem'}}>
+    <Timeline 
+            sx={{p: 0, m: 0, mb: 10,
+                [`& .${timelineItemClasses.root}:before`]: {
+                    flex: 0,
+                    padding: 0,
+                  }
+                }}>
+        <TimelineItem sx={{mx: 2}}>
             <TimelineSeparator>
                 <TimelineDot />              
             </TimelineSeparator>
-                <TimelineContent>
-                    <Typography>Permit created</Typography>
-                    <Typography sx={{fontSize: 12, color: 'rgba(0, 0, 0, 0.6)'}}>Saqib Sabir | Balfour Beatty</Typography>
-                
-                    <ArrowForwardIosRoundedIcon sx={{fontSize: 14, mr: '1rem'}} />
-        </TimelineContent>
+            <TimelineContent sx={{display: 'flex', justifyContent: 'space-between'}}>
+                <Stack direction="column" gap={0.5}>
+                <Typography variant="body1">Permit created</Typography>
+                <Typography variant="caption"
+                    // sx={{color: 'rgba(0, 0, 0, 0.6)'}}
+                    >Saqib Sabir | Balfour Beatty</Typography>
+                </Stack>
+                <ArrowForwardIosRoundedIcon sx={{fontSize: 14, m: '1rem'}} />
+            </TimelineContent>
         </TimelineItem>
 
     </Timeline>
-    
+    </Stack>
 
     </Box>
-</Box>
+
+</>
 )
 }
